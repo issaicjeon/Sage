e62d40b3ca45621f1cce299de6c254280482890d
issaicjeon <59120849+issaicjeon@users.noreply.github.com>
2020-11-29 22:03:43 -0500

    Odata

--- src/rest_controllers/odata.ts
+++ src/rest_controllers/odata.ts
@@ -1,5 +1,20 @@
-const odata = (URL: any, cache:any) => {
+var http = require('http');
 
+const odata = (URL: any, cache:any) => {
+    getURL(URL);
+    function getURL(URL:any) {
+    var body = '';
+    http.get(URL, function (res:any) {
+        res.on('data', function (chunk:any) {
+            body+=chunk;
+        });
+        res.on('end', function () {
+            console.log(body);
+        });
+    }).on('error', function(e:any) {
+        console.log('ERROR: ' + e.message);
+    });
+    }   
 }
 
 export default odata;
